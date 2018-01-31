cab32ae62ae8d906c39514760e00366223868417
NunoSpencer <nunspencer01@gmail.com>
2018-01-31 11:52:38 -0500

    added MySql query to submit request into database

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -1,3 +1,18 @@
-/*
-    this file is blank... content was included directly in index.html
-*/
+  /*
+      php code to connect to the database...
+  */
+  
+  
+  
+  <?php
+      $con = mysql_connect("localhost", "root", "");
+      $db = mysql_select_db("shovlrdb");
+
+      if($con)
+      {
+        echo("Connection sucessfully!");        
+      }else
+      {
+        die("Error connecting to Database!");
+      }
+  ?>
---
+++ php/submitquery.php
@@ -0,0 +1,23 @@
+/*
+    Database query to submit requests for snow shoveling... 
+    when user clicks SUBMIT button, the info from request.html form will be submitted to the database (on "Requests" table) 
+*/
+
+<?php
+        include_once("dbconnect.php");
+
+        $RequesterLName = $_POST["RequesterLName"];
+        $RequesterFName = $_POST["RequesterFName"];
+        $RequesterPhone = $_POST["RequesterPhone"];
+        $AreaSizeSqFT = $_POST["AreaSizeSqFT"];
+        $Street = $_POST["Street"];
+        $City = $_POST["City"];
+        $ZIP = $_POST["ZIP"];
+        //$PlowTruck = $_POST["PlowTruck"]; //this comes from radio-button selection... gotta know how to pass the value from radio-button
+        $Status = $_POST["pending"];
+
+        if(mysql_query("INSERT INTO requests VALUES ('$RequesterLName', '$RequesterFName', '$RequesterPhone', '$AreaSizeSqFT', '$Street', '$City','$ZIP', '$Status')"))
+            echo "Request was submitted sucessfully";
+        else
+            echo "Failed to querying into database!";
+?>
