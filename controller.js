62d36f2fa5cc438266422bc5a376009e156a5f7d
NunoSpencer <nunspencer01@gmail.com>
2018-02-01 15:19:31 -0500

    debuggin more...

--- controller.js
+++ controller.js
@@ -3,24 +3,6 @@
     this means to load the html content (i.e. the requester's info) to the database w/out the need to reload the html page...
 */
 
-/* version 1
-
-$("#submitButton").click( function() {                                      //when user clicks "Submit" button...
-    var data = $("#SubmitRequestForm : input").serializeArray() ;           //serialize the data from form to array "data"
-
-    $.post( $("#SubmitRequestForm").attr("action"), data, function(info) {  //do post and "ajaxify"...
-        $("#result").html(info);                                            //display output message from "submitquery.php" (i.e. echo "Request was submitted sucessfully" or echo "Failed to querying into database!")
-
-    } );
-});
-
-$("#SubmitRequestForm").submit( function() {                                //prevents from redirecting after submiting query...
-    return false;
-});
-
-*/ 
-
-
 // VERSION 2
 
 $(document).ready( function(e)
--- submitquery.php
+++ submitquery.php
@@ -3,29 +3,6 @@
     when user clicks SUBMIT button, the info from request.html form will be submitted to the database (on "Requests" table) 
 */
 
-
-/* VERSION 1
-<?php
-        include_once('dbconnect.php');
-
-        $RequesterLName = $_POST["RequesterLName"];
-        $RequesterFName = $_POST["RequesterFName"];
-        $RequesterPhone = $_POST["RequesterPhone"];
-        $AreaSizeSqFT = $_POST["AreaSizeSqFT"];
-        $Street = $_POST["Street"];
-        $City = $_POST["City"];
-        $ZIP = $_POST["ZIP"];
-        //$PlowTruck = $_POST["PlowTruck"]; //this comes from radio-button selection... gotta know how to pass the value from radio-button
-        $Status = $_POST["pending"];
-
-        if(mysqli_query("INSERT INTO requests VALUES ('$RequesterLName', '$RequesterFName', '$RequesterPhone', '$AreaSizeSqFT', '$Street', '$City','$ZIP', '$Status')"))
-            echo "Request was submitted sucessfully";
-        else
-            echo "Failed to querying into database!";
-?>
-*/
-
-
 //VERSION 2
 
 <?php
