cce77d35723a94e5e365f0d5a9c8f3a1bc6cf239
NunoSpencer <nunspencer01@gmail.com>
2018-02-06 12:45:11 -0500

    created query to display pending requests...

---
+++ displayrequests.php
@@ -0,0 +1,36 @@
+<?php
+    /*
+            Database query to display pending shovel requests... 
+            when user clicks "SHOVEL" button, a link with the number of requests is shown...
+            clicking the link will display a table all pending requests...
+    */
+
+    //connect to database (could also use "require (dbconnect.php);" so we don't have to code this everytime we need a connection")
+    $db_connection = mysqli_connect('localhost', 'root', '', 'shovlrdb');
+
+    //query to retrieve all pending requests
+    $selectQuery = "SELECT * FROM requests WHERE Status='pending'";
+
+    //query to display results
+    $result = mysqli_query($db_connection, $selectQuery) or die("Failed to querying into database!");
+
+
+    //display table headings
+    echo "<table border = '1'>";
+    echo "<tr> <td>Last Name</td> <td>First Name</td> <td>Phone</td> <td> Area Size</td> <td>Street</td> <td>City</td> <td>ZIP</td> <td>Status</td> </tr>";
+
+        while($row = mysqli_fetch_assoc($result))
+        {
+
+        }
+    
+    echo"</table>";
+
+
+
+
+
+
+
+
+?>
