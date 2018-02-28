7362bc56ea77ccbb567064febcfb8a91ce65d43f
NunoSpencer <nunspencer01@gmail.com>
2018-02-27 21:04:59 -0500

    created php file to search requests

---
+++ searchrequests.php
@@ -0,0 +1,16 @@
+<h1 id="headertitle" class=""><strong> :/  Under construction :/ Come back later ;) </strong></h1>
+
+
+<?php
+
+    /*
+        Database query to search for specific requests for snow shoveling... 
+        Search criteria includes: AreaSize (in sq.ft) and location (city, zip)
+
+        If input search matches records on the database, a query displays the found record, else display "Nothing found!" message
+    */
+
+    //connect to database
+    $db_connection = mysqli_connect('localhost', 'root', 'nopass123', 'shovlrdb');
+
+?>
